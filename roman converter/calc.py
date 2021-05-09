6312423dba784d6ea14d752ae684a00641caaeeb
Jonathan Yerushalmi <71553438+jonyeru23@users.noreply.github.com>
2021-05-09 22:36:57 +0300

    Update calc.py
    deaaling with negative numbers

--- roman converter/calc.py
+++ roman converter/calc.py
@@ -44,18 +44,25 @@ class ErrorChecker:
         else:
             raise UserWarning(f"The number to convert is too long can't calculate")
 
+    @staticmethod
+    def _is_positive(questionable_int, var_name):
+        if questionable_int > 0:
+            return True
+        else:
+            raise UserWarning(f"The {var_name} must be a positive number")
+
 
 class Calculator(ErrorChecker):
     def __init__(self, number_to_convert):
-        self.the_num = number_to_convert
-        self.roman = RomanSymbols()
-        self.roman_values = self.roman.get_roman_values()
-        self.roman_keys = self.roman.get_keys_in_order()
-        self.new_string = ''
+        self.__the_num = number_to_convert
+        self.__roman = RomanSymbols()
+        self.__roman_values = self.__roman.get_roman_values()
+        self.__roman_keys = self.__roman.get_keys_in_order()
+        self.__the_string = ''
 
     def _make_string(self):
-        if self._is_int(self.the_num, 'number_to_convert'):
-            return str(self.the_num)
+        if self._is_int(self.__the_num, 'number_to_convert') and self._is_positive(self.__the_num, 'number_to_convert'):
+            return str(self.__the_num)
 
     def _get_length(self):
         return len(self._make_string())
@@ -64,30 +71,30 @@ class Calculator(ErrorChecker):
         for power_of in reversed(range(self._is_it_too_long(self._get_length()))):
             part_of_num = self._get_dec(power_of)
             self._division(part_of_num)
-            self.the_num -= part_of_num
+            self.__the_num -= part_of_num
 
-        return self.new_string
+        return self.__the_string
 
     def _get_dec(self, power_of):
-        return self.the_num - (self.the_num % pow(10, power_of))
+        return self.__the_num - (self.__the_num % pow(10, power_of))
 
     def _division(self, part_of_num):
-        for index, symbol in enumerate(self.roman_keys):
-            times = self._how_many_times(part_of_num, self.roman_values[symbol])
-            if times == 0 and self._check_for_nine(part_of_num, self.roman_values[symbol]):
-                self.new_string += self.roman_keys[index + 2]
-                self.new_string += symbol
+        for index, symbol in enumerate(self.__roman_keys):
+            times = self._how_many_times(part_of_num, self.__roman_values[symbol])
+            if times == 0 and self._check_for_nine(part_of_num, self.__roman_values[symbol]):
+                self.__the_string += self.__roman_keys[index + 2]
+                self.__the_string += symbol
                 break
 
             elif self._is_between_4_and_0(times):
                 self._add_to_string(times, symbol)
                 if self._is_bigger_than_5(times):
-                    self._division(self._remainder(part_of_num, self.roman_values[symbol]))
+                    self._division(self._remainder(part_of_num, self.__roman_values[symbol]))
                 break
 
             elif self._is_it_four(times):
-                self.new_string += symbol
-                self.new_string += self.roman_keys[index - 1]
+                self.__the_string += symbol
+                self.__the_string += self.__roman_keys[index - 1]
 
             elif self._is_it_a_big_num(times, symbol):
                 self._add_to_string(times, symbol)
@@ -110,7 +117,7 @@ class Calculator(ErrorChecker):
 
     def _add_to_string(self, times, symbol):
         for _ in range(times):
-            self.new_string += symbol
+            self.__the_string += symbol
 
     @staticmethod
     def _how_many_times(part_of_num, roman_num):
@@ -123,4 +130,4 @@ class Calculator(ErrorChecker):
     @staticmethod
     def _check_for_nine(part_of_num, roman_num):
         return part_of_num / roman_num == 0.9
-    
+
